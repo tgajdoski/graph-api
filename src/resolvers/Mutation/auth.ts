5a5bfda52674f8a38c210cf030116f23db3bc3d5
tgajdoski <t.gajdoski@gmail.com>
2018-05-30 13:00:05 +0200

    login mutation extended with refreshToken - new mutation refreshtokens

--- src/resolvers/Mutation/auth.ts
+++ src/resolvers/Mutation/auth.ts
@@ -9,29 +9,25 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const Lodash = require("lodash");
 var serviceAccount = require("../../qnary-dev.json");
+const axios = require('axios');
 
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAL5jNakQk8PqF89f8rfS6TCedm0oFlBZ4",
-  authDomain: "qnary-dev.firebaseapp.com",
-  databaseURL: "https://qnary-dev.firebaseio.com",
-  messagingSenderId: "582735679903",
-  projectId: "qnary-dev",
-  storageBucket: "qnary-dev.appspot.com",
-  // apiKey: "AIzaSyDNDVQg4RncRKYr5r9O1GZrQOsv24NLd64",
-  // authDomain: "qnary-my-fb.firebaseapp.com",
-  // databaseURL: "https://qnary-my-fb.firebaseio.com",
-  // messagingSenderId: "582735679903",
-  // projectId: "qnary-my-fb",
-  // storageBucket: "qnary-my-fb.appspot.com",
+  apiKey: process.env.apiKey, 
+  authDomain: process.env.authDomain, 
+  databaseURL: process.env.databaseURL, 
+  messagingSenderId: process.env.messagingSenderId,
+  projectId: process.env.projectId, 
+  storageBucket: process.env.storageBucket, 
   credential: admin.credential.cert(serviceAccount)
 });
-// import { Context } from '../../utils'
+
 
 export const auth = {
  
 async login(parent, { email, password }, ctx, info) {
    const data = await firebase.auth().signInWithEmailAndPassword(email, password)
+   console.log('data user refresh ' , data.user.refreshToken);
    const user  = data.user;
   
     if (!user)
@@ -46,8 +42,22 @@ async login(parent, { email, password }, ctx, info) {
       })
     return {
         token: await user.getIdToken(true) , // jwt.sign({ userId: 'aaa' }, process.env.APP_SECRET),
+        refreshtoken: await user.refreshToken,
         user: userwithoid ,
         user_organizations: Object.keys(user_org).map(o => Object.assign({oid: o}, user_org[o]))
       }
   },
+   async refreshtokens(parent, { refreshToken }, ctx, info) {
+
+    let refUrl = `https://securetoken.googleapis.com/v1/token?key=${process.env.apiKey}&grant_type=refresh_token&refresh_token=${refreshToken}`
+    console.log('URL ' , refUrl);
+    var res = await axios.post(refUrl);
+    if (res.data)
+      return {
+        token:   res.data.access_token,
+        refreshtoken:   res.data.refresh_token,
+      }
+      throw new Error(`error: cant refresh tokens`);
+   },
+
 }
