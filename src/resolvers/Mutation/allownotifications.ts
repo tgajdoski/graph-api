de9388153561ecce10c7294c6236562d4eb7f6cc
tgajdoski <t.gajdoski@gmail.com>
2018-07-04 14:42:43 +0200

    clean up

--- src/resolvers/Mutation/allownotifications.ts
+++ src/resolvers/Mutation/allownotifications.ts
@@ -1,62 +1,62 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const Lodash = require('lodash');
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const Lodash = require("lodash");
 
 const rootRef = admin.database().ref();
 
 export const allownotifications_mutation = {
+  async allowNotification(_, { input, oid }, ctx) {
+    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`);
+    const userNotif = Lodash.pick(input, [
+      "id",
+      "app",
+      "platform",
+      "status",
+      "token",
+      "uid"
+    ]);
+    userNotif["created_at"] = admin.database.ServerValue.TIMESTAMP;
 
-   async allowNotification(_, {input, oid }, ctx) {
-    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
-   const userNotif =  Lodash.pick(input, ['id',  'app', 'platform', 'status', 'token', 'uid']);
-   userNotif['created_at'] =  admin.database.ServerValue.TIMESTAMP;
- 
     let updates = {
       [`notifications/${input.platform}/${input.device.id}`]: input,
       [`/user_notifications/${input.uid}/${input.token}`]: userNotif,
-      [`/organization_users/${oid}/${input.uid}/settings/notifications/push/off`]: false,
+      [`/organization_users/${oid}/${
+        input.uid
+      }/settings/notifications/push/off`]: false
     };
-    try{
-      let results = await rootRef.update(updates);
+    try {
+      await rootRef.update(updates);
       return true;
-    }
-    catch(error)
-    {
-      console.log('error :' , error );
-      throw error
+    } catch (error) {
+      console.log("error :", error);
+      throw error;
     }
   },
-  async checkAllowNotification(_, {uid, oid }, ctx) {
-    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
+  async checkAllowNotification(_, { uid, oid }, ctx) {
+    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`);
     let updates = {
-      [`/organization_users/${oid}/${uid}/settings/notifications/push/off`]: false,
+      [`/organization_users/${oid}/${uid}/settings/notifications/push/off`]: false
     };
-    try{
-      let results = await rootRef.update(updates);
+    try {
+      await rootRef.update(updates);
       return true;
-    }
-    catch(error)
-    {
-      console.log('error :' , error );
-      throw error
+    } catch (error) {
+      console.log("error :", error);
+      throw error;
     }
   },
-  async checkDissallowNotification(_, {uid, oid }, ctx) {
-    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
-  
+  async checkDissallowNotification(_, { uid, oid }, ctx) {
+    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`);
+
     let updates = {
-      [`/organization_users/${oid}/${uid}/settings/notifications/push/off`]: true,
+      [`/organization_users/${oid}/${uid}/settings/notifications/push/off`]: true
     };
-    try{
-      let results = await rootRef.update(updates);
+    try {
+      await rootRef.update(updates);
       return true;
+    } catch (error) {
+      console.log("error :", error);
+      throw error;
     }
-    catch(error)
-    {
-      console.log('error :' , error );
-      throw error
-    }
-  },
-
-
-}
+  }
+};
