587e27a5f10c80ca7d55b531f2ee1b273c6bbdc6
Toni Gajdoski <t.gajdoski@gmail.com>
2018-07-05 20:00:47 +0200

    org user connections

--- src/resolvers/Query/organization_user_connection.ts
+++ src/resolvers/Query/organization_user_connection.ts
@@ -4,15 +4,35 @@ const  { query } = require('../query-helper');
 const Lodash = require('lodash');
 
 const orgsusrconnRef = admin.database().ref('organization_user_connections');
+const connectionsRef = admin.database().ref('connections');
+
+
 
 export const organization_user_connections = {
   
-  organization_user_connections(_, { oid, uid }, ctx) {
+  async organization_user_connections(_, { oid, uid }, ctx) {
     const orgsRef = orgsusrconnRef.child(`${oid}/${uid}`);
-    return  query({}, ctx , orgsRef);
+    let oucs = await  query({}, ctx , orgsRef);
+    let output = [];
+    for( let ouc of oucs){
+      if(!ouc.id)
+       throw Error;
+      let id  = ouc.id;
+      let conn = await query({ id }, ctx , connectionsRef);
+      output.push(Lodash.assign(ouc, conn));
+    }
+    return output;
    },
-   organization_user_connection(_, { oid, uid, id }, ctx) {
+   async organization_user_connection(_, { oid, uid, id }, ctx) {
     const orgsRef  = orgsusrconnRef.child(`${oid}/${uid}`);
-    return  query({ id }, ctx , orgsRef);
+    let ouc = await  query({}, ctx , orgsRef);
+    console.log(ouc);
+     if(!ouc[0].id)
+       throw Error;
+    let oucid  = ouc[0].id;
+    console.log(oucid);
+    let conn = await query({ oucid }, ctx , connectionsRef);
+    return Lodash.assign(ouc[0], conn[0]);
+   // return  query({ id }, ctx , orgsRef);
    },
 }
